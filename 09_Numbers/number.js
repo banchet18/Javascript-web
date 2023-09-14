bfa7274fc94691fd6527c7651558efe5c9569f8e
banchet18 <65080829+banchet18@users.noreply.github.com>
2023-09-14 07:18:17 +0000

    changes

---
+++ 09_Numbers/number.js
@@ -0,0 +1,13 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const anotherNumber = 123.8966
+console.log(anotherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString("en-IN"));
