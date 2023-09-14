d197c3c7f9586551257fba3c925e4f1b041bd9ff
banchet18 <65080829+banchet18@users.noreply.github.com>
2023-09-14 06:11:52 +0000

    changes

---
+++ 04 _Operations/operations.js
@@ -0,0 +1,43 @@
+//****************************************************operations******************************************************** */
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = 'hello'
+let str2 = ' chetan'
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log('1'+2);
+// console.log(1+"2");
+
+// console.log('1' + 2 + 2); //not a good practice
+// console.log(1 + 2 + '2');
+
+// console.log(true);
+// console.log(+true); //again bad practice
+// console.log(+'');
+
+
+let num1, num2, num3
+
+num1 = num2 =num3 =100 //not a good practice
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter); 
+// search about ++ operator on mdn => assignment
+
+
+
